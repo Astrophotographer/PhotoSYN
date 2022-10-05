c00cb0db91926aed3aa3459b022e391897c63a38
elicho <elicho91@kakao.com>
2022-10-05 16:17:04 +0900

    Test

--- src/main/java/com/member/domain/WishListDTO.java
+++ src/main/java/com/member/domain/WishListDTO.java
@@ -1,2 +1,13 @@
-package com.member.domain;public class WishListDTO {
+package com.member.domain;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class WishListDTO {
+    private String U_ID;
+    private int G_NO;
+    private Timestamp W_REG;
+    private int M_IMGHQLY;
 }
--- src/main/java/com/member/mapper/WishListMapper.java
+++ src/main/java/com/member/mapper/WishListMapper.java
@@ -1,2 +1,20 @@
-package com.member.mapper;public class WishListMapper {
+package com.member.mapper;
+
+import com.member.domain.WishListDTO;
+
+import java.util.List;
+
+public class WishListMapper {
+
+    /* 카트 추가 */
+    public int addCart(WishListDTO wishList);
+
+    /* 카트 삭제 */
+    public int deleteCart(int cartId);
+
+    /* 카트 목록 */
+    public List<WishListDTO> getCart(String memberId);
+
+    /* 카트 확인 */
+    public WishListDTO checkCart(WishListDTO cart);
 }
