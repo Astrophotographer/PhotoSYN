d372d3558e477b5a7856c90a2cec1f1d0a19a0a6
j-yesung <hisung-ah@kakao.com>
2022-10-05 15:00:56 +0900

    Test_v1.1

--- src/main/java/com/member/controller/MypageController.java
+++ src/main/java/com/member/controller/MypageController.java
@@ -1,16 +1,11 @@
 package com.member.controller;
 
-import com.member.domain.MemberDTO;
 import com.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/member/mypage/*")
@@ -20,12 +15,8 @@ public class MypageController {
     private MemberService service;
 
     @GetMapping("profile")
-    public String profile(Model model, Authentication authentication, Principal principal) {
-        // 주석 처리 부분은 시큐리티 정보를 View 로 전송할 때 사용.
-        //UserDetails userDetails  = (UserDetails) authentication.getPrincipal();
+    public String profile(Model model) {
         String membership = "다이아";
-
-        //model.addAttribute("author", userDetails.getUsername());
         model.addAttribute("membership", membership);
 
         return "/member/mypage/profile";
