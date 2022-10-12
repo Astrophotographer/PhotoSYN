8f803f1005e8f3b7aa27164e73f80c6ca5ed7668
KimGyeJeong <pmwkdo@gmail.com>
2022-10-12 23:46:59 +0900

    admin단 구현시작

--- src/main/java/com/admin/controller/AdminController.java
+++ src/main/java/com/admin/controller/AdminController.java
@@ -4,11 +4,18 @@ import com.admin.service.AdminService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j
+@RequestMapping("/admin/*")
 public class AdminController {
 
     @Autowired
     private AdminService adminService;
+
+    @RequestMapping("main")
+    public String goMain(){
+        return "admin/main";
+    }
 }
