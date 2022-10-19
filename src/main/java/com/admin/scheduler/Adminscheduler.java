550e009af1e658c6f5784d40cfea694120165fce
KimGyeJeong <104893118+KimGyeJeong@users.noreply.github.com>
2022-10-19 17:42:26 +0900

    update.jsp 시큐리티 토큰들 넣어줌 + 스케쥴러 구현 중...2...

--- src/main/java/com/admin/scheduler/Adminscheduler.java
+++ src/main/java/com/admin/scheduler/Adminscheduler.java
@@ -21,28 +21,28 @@ public class Adminscheduler {
 
     //매일 정각?
 //    @Scheduled(cron = "0 0 0 * * *")
-    @Scheduled(cron="0 0/1 * * * *")
-    public void testPrint() throws  Exception{
-        log.info("===========================================");
-        log.info("testPrint");
-        log.info(new Date() + "실행!");
-
-        //모든 유저 아이디 들고와서
-//        List<Admin_MemberShip> member_With_uid = adminMapper.getUserList();
-        List<String> u_id = adminMapper.getUserList();
-        List<Admin_MemberShip> admin_memberShipList = null;
-
-        for(int i=0;i<u_id.size();i++){
-            log.info("u_id : "+u_id.get(i));
-            admin_memberShipList = adminMapper.getUserMemberList(u_id.get(i));
-
-        }
-
-        //유저 멤버쉽 비교를 위해 데이터들 가져옴.
-        for(int i=0; i<admin_memberShipList.size();i++){
-            log.info("admin_memberShipList : "+admin_memberShipList.get(i));
-        }
-    }
+//    @Scheduled(cron="0 0/1 * * * *")
+//    public void testPrint() throws  Exception{
+//        log.info("===========================================");
+//        log.info("testPrint");
+//        log.info(new Date() + "실행!");
+//
+//        //모든 유저 아이디 들고와서
+////        List<Admin_MemberShip> member_With_uid = adminMapper.getUserList();
+//        List<String> u_id = adminMapper.getUserList();
+//        List<Admin_MemberShip> admin_memberShipList = null;
+//
+//        for(int i=0;i<u_id.size();i++){
+//            log.info("u_id : "+u_id.get(i));
+//            admin_memberShipList = adminMapper.getUserMemberList(u_id.get(i));
+//
+//        }
+//
+//        //유저 멤버쉽 비교를 위해 데이터들 가져옴.
+//        for(int i=0; i<admin_memberShipList.size();i++){
+//            log.info("admin_memberShipList : "+admin_memberShipList.get(i));
+//        }
+//    }
 
     //매 5분마다 작동 --> 정상 작동
     @Scheduled(cron="0 0/5 * * * *")
