74f7b60cff45a08779b6c6c726cb361662533447
KimGyeJeong <104893118+KimGyeJeong@users.noreply.github.com>
2022-10-19 23:31:19 +0900

    스케쥴러 1단계 성공, 2단계 실패

--- src/main/java/com/admin/domain/Admin_MemberShip.java
+++ src/main/java/com/admin/domain/Admin_MemberShip.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class Admin_MemberShip {
 
     private String u_id;
-
+    private int u_status;
     private double sinceDate;
     private int galleryCount;
     private int sellerCount ;
